5f136deb9c59b0ad6f43ff419a57c18ac9d1db75
Minji So <sskkanji@gmail.com>
2024-03-28 13:52:49 +0900

    Update 자릿수더하기.js

--- level1/자릿수더하기.js
+++ level1/자릿수더하기.js
@@ -1,6 +1,3 @@
 function solution(n) {
-    const inputArray = n.toString().split("");
-    const answer = inputArray.reduce((accum, curr) => accum + Number(curr), 0);
-    
-    return answer;
+    return n.toString().split("").reduce((accum, curr) => accum + parseInt(curr), 0);
 }
