fd059ff592512a5b0877e311d14774295fd72781
Minji So <sskkanji@gmail.com>
2024-03-28 13:56:26 +0900

    Update 수박수박수박수박수박수?.js

--- level1/수박수박수박수박수박수?.js
+++ level1/수박수박수박수박수박수?.js
@@ -1,5 +1,6 @@
 function solution(n) { 
     let result = '';
-    Array(n).fill(null).forEach((_, i) => i % 2 === 0 ? result+='수' : result+='박');
+    Array(n).fill(null).forEach((_, i) => i % 2 === 0 ? result += '수' : result += '박');
+    
     return result;
 }
