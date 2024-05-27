f41802eef67662fecab2c119c47e9c4e853ae6df
Minji So <sskkanji@gmail.com>
2024-05-27 14:34:49 +0900

    Update 약수구하기.js

--- level1/약수구하기.js
+++ level1/약수구하기.js
@@ -1,11 +1,11 @@
 function solution(n) {
     let result = 0;
     
-    for (let i=0; i<=n; i++) {
-        if (i !== 0 && n % i === 0) {
+    for (let i=1; i<=n; i++) {
+        if (n % i === 0) {
             result += i;
         }
     }
-  
+
     return result;
 }
